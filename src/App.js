ddf7b0adad6e333425236b09c0f448038300759c
shuaibu1300 <shuaibu1300@gmail.com>
2026-07-02 22:55:31 +0100

    fix PDF open

--- src/App.js
+++ src/App.js
@@ -518,7 +518,7 @@ const [schoolClasses, setSchoolClasses] = useState([]);
           const data = JSON.parse(xhr.responseText);
           await addDoc(collection(db, collectionName), {
             title,
-            const finalURL = collectionName === "pdfs" ? data.secure_url.replace("/image/upload/", "/raw/upload/") : data.secure_url;
+            const finalURL = collectionName === "pdfs" ? data.secure_url.replace("/image/upload/", "/raw/upload/fl_attachment/") : data.secure_url;
             url: finalURL,
             createdAt: new Date()
           });
